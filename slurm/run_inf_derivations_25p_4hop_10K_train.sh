01d61dfad9a7f932c6c58cc6bb753eca2fbe5289
ariyaninf <115722162+ariyaninf@users.noreply.github.com>
2024-07-17 11:58:35 +0100

    Add file run_inf_der_25p_4h_10K_train via upload

---
+++ slurm/run_inf_derivations_25p_4hop_10K_train.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=inf_gen_steps_25p4h_10K_train
+#SBATCH --error=log/e.%x.%j
+#SBATCH --output=log/o.%x.%j
+#SBATCH --partition=gpu_v100
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=48:00:00
+#SBATCH --account=scw1997
+
+set -eu
+
+module purge
+module load anaconda
+module list
+
+source activate
+source activate myenv
+
+WORKDIR=/scratch/$USER/LlamaFinetuning
+cd ${WORKDIR}
+
+python3 inf_gen_derivations.py \
+  --dataset_name 2sat_25mixVars_25fixCls_4-hop_10K_INC_500_OR_train \
+  --dataset_dir dataset/Derivations_INC \
+  --model_id output/gen_Derivations/unsloth/llama-3-8b-Instruct-bnb-4bit/2sat_25mixVars_25fixCls_4-hop_10K_500_OR_prompt_5/lora_model \
+  --output_dir output/test_Derivations \
+  --prompt_type 5 \
+  --max_new_tokens 500
+
+
+
+
